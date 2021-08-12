f5e6a58296fd25867ccd32f9c71d3ae41f799906
Andrew <andrew.eppinger@yahoo.com>
2021-08-11 22:22:53 -0400

    Reverts to initial code

--- 361microservice.py
+++ 361microservice.py
@@ -1,16 +1,19 @@
 #This is the headline, noun, and verb generator microservice for CS_361
 
 from flask import Flask
-
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
-    """
+
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
