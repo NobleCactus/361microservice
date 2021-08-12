fa15d0a552e291b586444db9bc1a392693cf8e6e
Andrew <andrew.eppinger@yahoo.com>
2021-08-11 23:32:50 -0400

    testing

--- app.py
+++ app.py
@@ -9,23 +9,12 @@ def respond():
 
     return "Noun"
 
-@app.route('/post/', methods=['POST'])
+@app.route('/verb', methods=['GET'])
 def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+   def verb_response():
 
-# A welcome message to test our server
+       return "Verb"
+
 @app.route('/', methods=['GET'])
 def index():
     horse = "HORSE"
